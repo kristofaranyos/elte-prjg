37f5c5b79e946b6569a83f6acfb454d035336172
kristofaranyos <hivua5@inf.elte.hu>
2020-02-23 23:52:25 +0100

    Initialized back and frontend

---
+++ backend/src/main/java/hu/elte/prjgbackend/PrjgBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.prjgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjgBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/elte/prjgbackend/PrjgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.prjgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
