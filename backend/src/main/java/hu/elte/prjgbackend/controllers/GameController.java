806a6cf3c522883df29526d282392f41fc828ada
Jacint <chopperboy201@gmail.com>
2020-03-11 19:00:18 +0100

    start game included

--- backend/src/main/java/hu/elte/prjgbackend/controllers/GameController.java
+++ backend/src/main/java/hu/elte/prjgbackend/controllers/GameController.java
@@ -1,6 +1,8 @@
 package hu.elte.prjgbackend.controllers;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import hu.elte.prjgbackend.models.Game;
@@ -23,5 +25,25 @@ public class GameController{
     public ResponseEntity<Iterable<Game>> getGames(){
         Iterable<Game> games = gameRepository.findAll();
         return ResponseEntity.ok(games);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity startGame(@RequestParam Long id) {
+        if (id == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        try{
+
+            Game game = gameRepository.findById(id).get();
+            game.setIsRunning(true);
+            game.setStartedDate(Instant.now().toEpochMilli());
+            gameRepository.save(game);
+
+            return ResponseEntity.ok().build();
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
